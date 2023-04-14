9e117341b43305ec2282c61cef1443750a8cb525
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-04-14 13:04:44 +0100

    modifications

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL, Email
+from wtforms.validators import Email
+from wtforms.fields.html5 import EmailField
 from flask_ckeditor import CKEditorField
 
 ##WTForm
@@ -13,12 +15,12 @@ class CreatePostForm(FlaskForm):
 
 class Register(FlaskForm):
     name = StringField('Name:', validators=[DataRequired()])
-    email = StringField('Email>:', validators=[DataRequired(), Email()])
+    email = EmailField('Email>:', validators=[DataRequired(), Email()])
     password = PasswordField('Password: ', validators=[DataRequired()])
     submit = SubmitField("Register")
 
 class Login(FlaskForm):
-    email = StringField('Email>:', validators=[DataRequired(), Email()])
+    email = EmailField('Email>:', validators=[DataRequired(), Email()])
     password = PasswordField('Password: ', validators=[DataRequired()])
     submit = SubmitField("Login")
 
